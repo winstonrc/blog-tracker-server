683f106ebe9d5b0c00fbcc0468facb69d26b2578
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-06-11 11:32:57 -0500

    update through 2.2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,54 +1,29 @@
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
-
-const Content = (props) => {
-  console.log(props)
-  return <p>
-    {props.parts[0].name} {props.parts[0].exercises}
-    <br></br>
-    {props.parts[1].name} {props.parts[1].exercises}
-    <br></br>
-    {props.parts[2].name} {props.parts[2].exercises}
-  </p>
-  
-}
-
-const Total = (props) => {
-  console.log(props)
-  return <p>Number of exercises {
-    props.parts[0].exercises + 
-    props.parts[1].exercises + 
-    props.parts[2].exercises}</p>
-}
+import Course from "./components/Course"
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
