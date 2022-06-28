1407792cf822e47571d40d0a5853a378c6903636
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-06-28 15:53:37 -0500

    add delete functionality

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Person from './Person'
 
-const Persons = ({personsToShow}) => {
+const Persons = ({personsToShow, deletePerson}) => {
     return (
         <div>
         {personsToShow.map(person =>
-          <Person key={person.name} person={person}/>
+          <Person 
+            key={person.id} 
+            person={person}
+            deletePerson={() => deletePerson(person.id)}
+          />
         )}
       </div>
     )
