2f5fd10b1b4a60e61bd5e91ffd8c63ac0b4c06ff
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-06-28 17:42:25 -0500

    Add style

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,10 +1,25 @@
-const Notification = ({ message }) => {
+const Notification = ({ message, greenMessage }) => {
+    let messageColor = 'green'
+    if (greenMessage) {
+        messageColor = 'green'
+    } else messageColor = 'red' 
+
+    const notificationStyle = {
+        color: messageColor,
+        background: 'lightgray',
+        fontSize: 20,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    }
+
     if (message === null) {
         return null
     }
 
     return (
-        <div className="message">
+        <div style={notificationStyle}>
             {message}
         </div>
     )
