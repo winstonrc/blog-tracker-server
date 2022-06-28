93e404cc4040b2d0d05c4d562c9c33aeedaa6137
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-06-28 16:25:34 -0500

    rename method

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -16,9 +16,9 @@ const update = (id, newObject) => {
     return request.then(response => response.data)
 }
 
-const deletePerson = (id) => {
+const remove = (id) => {
     const request = axios.delete(`${baseURL}/${id}`)
     return request.then(response => response.data)
 }
 
-export default { getAll, create, update, deletePerson }
+export default { getAll, create, update, remove }
