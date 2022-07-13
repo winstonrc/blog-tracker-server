df5447b02ff9e9a94b79e84ae359537b3d3f03c4
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-13 11:01:59 -0500

    implement functional good, ok, bad buttons

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -11,16 +11,34 @@ const App = () => {
       type: 'GOOD'
     })
   }
+  
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'RESET'
+    })
+  }
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
+      <button onClick={good}>good</button> &nbsp;
+      <button onClick={ok}>ok</button> &nbsp;
+      <button onClick={bad}>bad</button> &nbsp;
       <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>good: {store.getState().good}</div>
+      <div>ok: {store.getState().ok}</div>
+      <div>bad: {store.getState().bad}</div>
     </div>
   )
 }
