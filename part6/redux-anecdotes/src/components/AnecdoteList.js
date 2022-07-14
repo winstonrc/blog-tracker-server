554f8af37589c5cc6cfc4aeecd57fe76c9ae8340
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-13 20:27:19 -0500

    update formatting

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -9,13 +9,13 @@ const AnecdoteList = () => {
         <div>
             {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
                 <div key={anecdote.id}>
-                <div>
-                    {anecdote.content}
-                </div>
-                <div>
-                    Votes: {anecdote.votes} &nbsp;
-                    <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
-                </div>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        Votes: {anecdote.votes} &nbsp;
+                        <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+                    </div>
                 </div>
             )}
         </div>
