7c4af5c04ef0ebdf916997e57bcd634210a4c371
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-13 20:27:55 -0500

    update formatting

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -15,7 +15,9 @@ const AnecdoteForm = () => {
         <div>
         <h2>create new</h2>
         <form onSubmit={createAnecdote}>
-            <div><input name='anecdote'/></div>
+            <div>
+                <input name='anecdote'/>
+            </div>
             <button type='submit'>create</button>
         </form>
       </div>
