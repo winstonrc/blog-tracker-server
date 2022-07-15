7452220f67f2db1a4fb5b03dab1537dce8a5da21
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-14 19:57:24 -0500

    update through 6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,10 +2,20 @@ import Filter from './components/Filter'
 import Notification from './components/Notification'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
-import { useSelector } from 'react-redux'
+import anecdotesService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const App = () => {
   const notification = useSelector(state => state.notification)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdotesService.getAll().then(anecdotes => 
+      dispatch(setAnecdotes(anecdotes))
+    )
+  }, [dispatch])
 
   return (
     <div>
