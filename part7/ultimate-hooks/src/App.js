78c60e2e1a884e5ec9d0782608ebd4f47120d1fe
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-18 17:07:57 -0500

    update through 7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,22 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    
+    try {
+      fetchData()
+    } catch (error) {
+      console.log(error)
+    }
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
