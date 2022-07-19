40d60b919474ff4333c6f7b085b11ca201b33cf0
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-19 11:09:10 -0500

    extract button click into separate function

--- part7/webpack/src/App.js
+++ part7/webpack/src/App.js
@@ -3,11 +3,17 @@ import './index.css'
 
 const App = () => {
     const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState()
+
+    const handleClick = () => {
+        setCounter(counter + 1)
+        setValues(values.concat(counter))
+    }
 
     return (
         <div className='container'>
             hello webpack {counter} clicks &nbsp;
-            <button onClick={() => setCounter(counter + 1)}>
+            <button onClick={handleClick}>
                 press
             </button>
         </div>
