3df53a4c176c1a5327eebb215714fa4ca84eaa40
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-19 11:02:56 -0500

    add React to runtime

--- part7/webpack/webpack.config.js
+++ part7/webpack/webpack.config.js
@@ -6,13 +6,21 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
   },
+  devServer: {
+    static: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            '@babel/preset-env',
+            ['@babel/preset-react', {'runtime': 'automatic'}]
+          ],
         },
       },
       {
