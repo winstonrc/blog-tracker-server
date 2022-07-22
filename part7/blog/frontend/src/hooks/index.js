a5de13e482f699857bd0729b764b8c003f42195b
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-22 13:45:14 -0500

    remove unused hooks

--- part7/blog/frontend/src/hooks/index.js
+++ part7/blog/frontend/src/hooks/index.js
@@ -20,39 +20,3 @@ export const useField = (type) => {
     reset,
   };
 };
-
-export const useLabel = (type, defaultValue, toggledValue) => {
-  const [value, setValue] = useState(defaultValue);
-
-  const toggle = () => {
-    if (value === defaultValue) {
-      setValue(toggledValue);
-    } else {
-      setValue(defaultValue);
-    }
-  };
-
-  return {
-    type,
-    value,
-    toggle,
-  };
-};
-
-export const useBoolean = (type) => {
-  const [value, setValue] = useState(false);
-
-  const toggle = () => {
-    if (value === false) {
-      setValue(true);
-    } else {
-      setValue(false);
-    }
-  };
-
-  return {
-    type,
-    value,
-    toggle,
-  };
-};
