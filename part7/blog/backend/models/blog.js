529c23249e9ed886d1294bd6c8d06209ecd9f597
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-22 14:51:39 -0500

    update formatting

--- part7/blog/backend/models/blog.js
+++ part7/blog/backend/models/blog.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-param-reassign */
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
   title: {
@@ -15,13 +15,16 @@ const blogSchema = new mongoose.Schema({
     required: true,
   },
   likes: Number,
+  comments: {
+    type: String,
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: 'User',
   },
 });
 
-blogSchema.set("toJSON", {
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
@@ -29,4 +32,4 @@ blogSchema.set("toJSON", {
   },
 });
 
-module.exports = mongoose.model("Blog", blogSchema);
+module.exports = mongoose.model('Blog', blogSchema);
