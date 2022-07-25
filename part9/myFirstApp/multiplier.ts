5d88a74ccccfc13b9d264669dd5d042bcdf8406a
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-25 16:23:31 -0500

    add types

--- part9/myFirstApp/multiplier.ts
+++ part9/myFirstApp/multiplier.ts
@@ -1,5 +1,5 @@
-const muliplicator = (a, b, printText) => {
+const muliplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
-}
+};
 
-muliplicator(2, 4, 'Multiplied numbers 2 and 4, the result is: ')
+muliplicator(2, 4, 'Multiplied numbers 2 and 4, the result is: ');
