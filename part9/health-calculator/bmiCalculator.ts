59e934092c911910e9a13e596cda0fd50dc5f710
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-26 11:24:54 -0500

    9.5

--- part9/health-calculator/bmiCalculator.ts
+++ part9/health-calculator/bmiCalculator.ts
@@ -1,64 +1,72 @@
-interface bmiValues {
-  mass: number;
-  height: number;
-  units: string;
-}
+const bmiCategories = [
+  'Underweight (Severe thinness)',
+  'Underweight (Moderate thinness)',
+  'Underweight (Mild thinness)',
+  'Normal range',
+  'Overweight (Pre-obese)',
+  'Obese (class I)',
+  'Obese (class II)',
+  'Obese (class III)',
+];
 
-const parseArguments = (args: Array<string>): bmiValues => {
-  if (args.length < 5) throw new Error('Not enough arguments');
-  if (args.length > 5) throw new Error('Too many arguments');
+const calculateBmi = (height: number, weight: number, unit: string) => {
+  if (height !== 0 && weight !== 0) {
+    const bmi = weight / Math.pow(height, 2);
+    // const bmi = ((weight / (height * height)) * 100) / 100;
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      mass: Number(args[2]),
-      height: Number(args[3]),
-      units: args[4],
-    };
-  } else throw new Error('Provided values must be numbers!');
-};
-
-const calculateBmi = (mass: number, height: number, units: string): number => {
-  if (height !== 0 && mass !== 0) {
-    switch (units) {
+    switch (unit) {
       case 'metric':
-        return Math.round((mass / (height * height)) * 100) / 100;
+        return analyzeBmi(bmi);
+      // return Math.round((weight / (height * height)) * 100) / 100;
       case 'imperial':
-        return Math.round(((mass * 703) / (height * height)) * 100) / 100;
+        return analyzeBmi(bmi * 703);
+      // return Math.round(((weight * 703) / (height * height)) * 100) / 100;
       default:
-        throw new Error('Provided unit must be either kg or lb');
+        throw new Error('Provided unit must be either metric or imperial');
     }
   } else throw new Error('Cannot divide by 0!');
 };
 
 const analyzeBmi = (bmi: number): string => {
+  let prefix = '';
+
   if (bmi < 16) {
-    return 'Underweight (Severe thinness)';
+    prefix = bmiCategories[0];
   } else if (bmi < 17) {
-    return 'Underweight (Moderate thinness)';
+    prefix = bmiCategories[1];
   } else if (bmi < 18.5) {
-    return 'Underweight (Mild thinness)';
+    prefix = bmiCategories[2];
   } else if (bmi < 25) {
-    return 'Normal range';
+    prefix = bmiCategories[3];
   } else if (bmi < 30) {
-    return 'Overweight (Pre-obese)';
+    prefix = bmiCategories[4];
   } else if (bmi < 35) {
-    return 'Obese (class I)';
+    prefix = bmiCategories[5];
   } else if (bmi < 40) {
-    return 'Obese (Class II)';
+    prefix = bmiCategories[6];
   } else {
-    return 'Obese (Class III)';
+    prefix = bmiCategories[7];
   }
+
+  return `BMI: ${bmi.toFixed(2)} - ${prefix}`;
 };
 
-try {
-  const { mass, height, units } = parseArguments(process.argv);
-  const bmi = calculateBmi(mass, height, units);
-  console.log(`BMI: ${bmi}`);
-  console.log(analyzeBmi(bmi));
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong.';
-  if (error instanceof Error) {
-    errorMessage += ` Error: ${error.message}`;
+export const parseAndCalculateBMI = (arg1: any, arg2: any, arg3: any) => {
+  console.log('arg1', arg1);
+  console.log('arg2', arg2);
+  console.log('arg3', arg3);
+
+  if (!arg1 || !arg2 || !arg3) {
+    throw new Error(
+      'Not enough arguments. Provide Height, Weight, and Unit respectively.'
+    );
   }
-  console.log(errorMessage);
-}
+
+  const height = parseFloat(arg1);
+  const weight = parseFloat(arg2);
+  const unit = arg3;
+
+  if (!isNaN(Number(arg1)) && !isNaN(Number(arg2))) {
+    return calculateBmi(height, weight, unit);
+  } else throw new Error('Height and Weight values must be numbers!');
+};
--- part9/health-calculator/index.ts
+++ part9/health-calculator/index.ts
@@ -1,10 +1,39 @@
 import express from 'express';
-const app = express();
+import { parseAndCalculateBMI } from './bmiCalculator';
 
-const PORT = 3003;
+const app = express();
+const PORT = 3002;
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = req.query.height;
+  const weight = req.query.weight;
+  const unit = req.query.unit;
+  const bmi = parseAndCalculateBMI(height, weight, unit);
+
+  let heightAppendix = '';
+  if (unit === 'metric') {
+    heightAppendix = 'm';
+  } else if (unit === 'imperial') {
+    heightAppendix = 'in';
+  }
+
+  let weightAppendix = '';
+  if (unit === 'metric') {
+    weightAppendix = 'kg';
+  } else if (unit === 'imperial') {
+    weightAppendix = 'lb';
+  }
+
+  res.send({
+    height: `${height} ${heightAppendix}`,
+    weight: `${weight} ${weightAppendix}`,
+    unit,
+    bmi,
+  });
 });
 
 app.listen(PORT, () => {
