919f8ae9ea1bf7de95253975089e68f0c575f049
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-26 16:28:20 -0500

    add cors

--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const cors = require('cors');
+
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged');
