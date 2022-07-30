5c84e3b94ff11d84b19c6db3fb233fcf8278a8f4
Winston Cooke <71997294+winstoncooke@users.noreply.github.com>
2022-07-30 12:58:47 -0500

    update for deployment

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const http = require("http");
-const app = require("./app");
-const config = require("./utils/config");
-const logger = require("./utils/logger");
+const http = require('http');
+const app = require('./app');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
 const server = http.createServer(app);
+
+app.get('*', (_req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/'))
+    // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
